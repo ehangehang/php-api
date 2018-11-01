aa1e9a1c408a69673a2e11749d1fb08996e899b7
ehangehang <rayhansuryatama@gmail.com>
2018-11-01 19:56:21 +0700

    fourth

--- index.php
+++ index.php
@@ -14,22 +14,26 @@
     <th>Password</th>
     <th>Level</th>
     <th>Fullname</th>
+  <th>Action</th>
   </tr>
   </div>
   <?php
-  include('connect.php'); //membuat koneksi
-  $sql_tampil ="SELECT * FROM tabel_kk4"; //perintah SQL untuk menampilkan tabel
-  $peserta=mysqli_query($conn,$sql_tampil); //jalankan dengan mySQL Query
-  while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){ //lakukan fetch dengan result type MYSQL_ASSOC
-    echo'
+  include('connect.php');// 1. Lakukan include koneksi.php untuk membuat koneksi
+  $sql_tampil ="SELECT * FROM tabel_kk4";// 2. Buat perintah SQL untuk menampilkan data
+  $peserta=mysqli_query($conn,$sql_tampil);// 3. Jalankan perintah diatas dengan fungsi mysqli_query
+  while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){// 4. Lakukan fetch dengan result type MYSQL_ASSOC
+   ?> 
     </tr>
-      <td>'.$baris_data['id'].'</td>
-      <td>'.$baris_data['username'].'</td>
-      <td>'.$baris_data['password'].'</td>
-      <td>'.$baris_data['level'].'</td>
-      <td>'.$baris_data['fullname'].'</td>
-    </tr>';
-  }
-  ?>
+      <td><?php echo $baris_data['id']; ?></td>
+      <td><?php echo $baris_data['username']; ?></td>
+      <td><?php echo $baris_data['password']; ?></td>
+      <td><?php echo $baris_data['level']; ?></td>
+      <td><?php echo $baris_data['fullname']; ?></td>
+      <td>
+      <a class="edit" href="edit.php?Id=<?php echo $baris_data['Id']; ?>">Edit</a> |
+      <a class="hapus" href="delete.php?Id=<?php echo $baris_data['Id']; ?>">Delete</a>
+      </td>
+    </tr>
+  <?php } ?>
 </body>
 </html>
